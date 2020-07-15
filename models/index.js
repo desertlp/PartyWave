40694122decd534b89aec59f70f3113e79b3aef4
wadams <wwadams94@gmail.com>
2020-07-15 14:46:28 -0700

    updated fonts

--- models/index.js
+++ models/index.js
@@ -1,22 +1,24 @@
-const mongoose = require('mongoose'); // import mongoose and its methods
-const MONGODB_URI = 'mongodb://localhost:27017/SOCALSURF'
+const mongoose = require("mongoose"); // import mongoose and its methods
+const MONGODB_URI = "mongodb://localhost:27017/SOCALSURF";
 
-mongoose.connect(MONGODB_URI, { // mongodb is a secondary server, must connect to it liketwe do with server.js
-     // first argument: connectionstring
+mongoose
+  .connect(MONGODB_URI, {
+    // mongodb is a secondary server, must connect to it liketwe do with server.js
+    // first argument: connectionstring
     // second argument: deprecation warnings (google this often)
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false,    
-})
-    .then(() => {
-        console.log('MongoDB connected');
-    })
-    .catch((err) => console.log(`MongoDB connection failed: ${err}`));
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log(`MongoDB connection failed: ${err}`));
 
 module.exports = {
-    BEACH: require('./Beach'), 
-    COUNTY: require('./County'),
-    COMMENT: require('./Comment'),
-    USER: require('./User'),
+  BEACH: require("./Beach"),
+  COUNTY: require("./County"),
+  COMMENT: require("./Comment"),
+  USER: require("./User"),
 };

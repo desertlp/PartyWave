4b348e3b52751e4a951a6a1522f4300d1addee39
desertlp <lorapaine1@gmail.com>
2020-07-27 18:46:15 -0700

    Public > public

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const commentsController = require("./controllers/commentsController");
 const usersController = require("./controllers/usersController");
 const authController = require("./controllers/authController");
 const apiController = require("./controllers/apiController");
+
 // Set View Engine
 app.set("view engine", "ejs");
 
@@ -50,6 +51,48 @@ app.use("/liked", apiController);
 
 //404 ROUTE
 app.get("*", (req, res) => {res.send("<h1>404 Error.... Page Not Found.</h1>")});
+
+// COUNTY DATA
+
+/* const county = [
+  {
+      name: 'Los Angeles County',
+      lattitude: '34.0522° N', 
+      longitude: '1118.2437° W', 
+  },
+  {
+      name: 'Orange County',
+      lattitude: '33.7175° N', 
+      longitude: '117.8311° W', 
+  },
+  {
+      name: 'San Diego County',
+      lattitude: '32.7157° N', 
+      longitude: '117.1611° W', 
+  },
+  {
+      name: 'San Luis Obispo County',
+      lattitude: '35.3102° N', 
+      longitude: '120.4358° W', 
+  },
+  {
+      name: 'Santa Barbara County ',
+      lattitude: '34.4208° N', 
+      longitude: '119.6982° W',
+  },
+  {
+      name: 'Ventura County',
+      lattitude: '34.3705° N', 
+      longitude: '119.1391° W',
+  },
+];
+
+db.COUNTY.create(county, (err, newCounty) => {
+  if (err) console.log(err);
+  console.log('New County Created');
+  console.log(newCounty);
+  process.exit();
+});     */
 
 // SERVER LISTENER
 app.listen(PORT, console.log(`SERVER RUNNING ON PORT ${PORT}`));
